1945fff065ee3b6579e3b3f213a8bf1f79ccd98b
DESKTOP-8MJ3UPB\alfas <aa>
2022-05-26 10:41:48 +0200

    Estructura anuncios pagina Inicio

--- ProyectFinal/src/Controller/FXMLDocumentController.java
+++ ProyectFinal/src/Controller/FXMLDocumentController.java
@@ -10,6 +10,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 /**
@@ -19,6 +20,44 @@ import javafx.scene.control.Label;
 public class FXMLDocumentController implements Initializable {
 
     @FXML
+    private Label tituloAnuncio2;
+    @FXML
+    private Label descripcionAnuncio2;
+    @FXML
+    private Label categoriaAnuncio2;
+    @FXML
+    private Label tiempoAnuncio2;
+    @FXML
+    private Label numParticipantes2;
+    @FXML
+    private Label ubicacionAnuncio2;
+    @FXML
+    private Label tituloAnuncio1;
+    @FXML
+    private Label descripcionAnuncio1;
+    @FXML
+    private Label categoriaAnuncio1;
+    @FXML
+    private Label tiempoAnuncio1;
+    @FXML
+    private Label numParticipantes1;
+    @FXML
+    private Label ubicacionAnuncio1;
+    @FXML
+    private Button inscribirseAnuncio1;
+    @FXML
+    private Label tituloAnuncio3;
+    @FXML
+    private Label descripcionAnuncio3;
+    @FXML
+    private Label categoriaAnuncio3;
+    @FXML
+    private Label tiempoAnuncio3;
+    @FXML
+    private Label numParticipantes3;
+    @FXML
+    private Label ubicacionAnuncio3;
+
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
 
